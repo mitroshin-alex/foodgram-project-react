293bba2e68a80bc9eeff16206d118390a504bb12
Aleksey Mitroshin <mitroshin.alex@gmail.com>
2022-10-24 23:27:35 +0300

    FIX filters.py

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,6 +1,6 @@
 import django_filters
 
-from recipes.models import Recipe, Ingredient
+from recipes.models import Recipe, Ingredient, Tag
 
 
 class IngredientFilter(django_filters.FilterSet):
@@ -16,7 +16,11 @@ class IngredientFilter(django_filters.FilterSet):
 class RecipeFilter(django_filters.FilterSet):
     """Фильтр рецептов."""
     author = django_filters.NumberFilter(field_name='author__id')
-    tags = django_filters.AllValuesMultipleFilter(field_name='tags__slug')
+    tags = django_filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        queryset=Tag.objects.all(),
+        to_field_name='slug',
+    )
     is_favorited = django_filters.NumberFilter(method='filter_is_favorited')
     is_in_shopping_cart = django_filters.NumberFilter(
         method='filter_is_in_shopping_cart'
